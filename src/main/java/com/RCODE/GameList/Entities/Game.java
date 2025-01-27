ec6a6c708f9b9f58911aa56b43b2c505ccd522d0
Harunookami <rafinha-2410@hotmail.com>
2025-01-26 21:47:21 -0300

    Game seed

--- src/main/java/com/RCODE/GameList/Entities/Game.java
+++ src/main/java/com/RCODE/GameList/Entities/Game.java
@@ -21,10 +21,16 @@ public class Game {
 
     @Column(name = "game_year")
     private Integer year;
+
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -40,7 +46,8 @@ public class Game {
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -78,12 +85,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
